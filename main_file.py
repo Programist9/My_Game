36a6feee6fd3aa9dd3d437ee0d270a802f39446f
unknown <pandachka874@gmail.com>
2024-04-06 13:20:57 +0300

    added files

--- main_file.py
+++ main_file.py
@@ -1,72 +1,22 @@
-import pygame
-import sys
-
-# Инициализация Pygame
+import pygame 
 pygame.init()
 
-# Константы
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-FPS = 60
-
-# Создание окна
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Простой платформер")
-
-# Класс для игрока
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(WHITE)
-        self.rect = self.image.get_rect()
-        self.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-        self.vel = pygame.math.Vector2(0, 0)
-        self.acc = pygame.math.Vector2(0, 0)
-        self.jump = False
-
-    def update(self):
-        keys = pygame.key.get_pressed()
-        self.acc = pygame.math.Vector2(0, 0.5)
-
-        if keys[pygame.K_LEFT]:
-            self.acc.x = -0.5
-        if keys[pygame.K_RIGHT]:
-            self.acc.x = 0.5
-
-        self.acc.x += self.vel.x * -0.12
-        self.vel += self.acc
-        self.rect.x += self.vel.x + 0.5 * self.acc.x
-        self.rect.y += self.vel.y + 0.5 * self.acc.y
-
-        if self.rect.left > SCREEN_WIDTH:
-            self.rect.right = 0
-        if self.rect.right < 0:
-            self.rect.left = SCREEN_WIDTH
-
-# Группа спрайтов
-all_sprites = pygame.sprite.Group()
-player = Player()
-all_sprites.add(player)
-
-# Главный цикл игры
+win_w, win_h = 700, 500
+window=pygame.display.set_mode((win_w, win_h))
+fps=60
 clock = pygame.time.Clock()
-running = True
-while running:
+
+pygame.mixer_music.load("snd/No More Magic.ogg")
+pygame.mixer_music.play(-1)
+pygame.mixer_music.set_volume(0.2)
+
+background = pygame.image.load("img/background.png")
+background = pygame.transform.scale(background,(win_w, win_h))
+game = True
+while game:
+    window.blit(background,(0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-
-    all_sprites.update()
-
-    # Отрисовка
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
-
-    pygame.display.flip()
-    clock.tick(FPS)
-
-pygame.quit()
-sys.exit()
+            game = False
+    pygame.display.update()
+    clock.tick(fps)
