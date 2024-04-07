03dccc94008ce77232ef3d264cf56b362e9099c7
unknown <pandachka874@gmail.com>
2024-04-07 13:09:00 +0300

    updated

--- main_file.py
+++ main_file.py
@@ -1,22 +1,31 @@
 import pygame 
 pygame.init()
-
+# Создание окна
 win_w, win_h = 700, 500
+pygame.display.set_caption("Платформер")
 window=pygame.display.set_mode((win_w, win_h))
-fps=60
+FPS = 60
 clock = pygame.time.Clock()
-
-pygame.mixer_music.load("snd/No More Magic.ogg")
+# Музыка
+pygame.mixer_music.load("backgroundMusic.mp3")
 pygame.mixer_music.play(-1)
 pygame.mixer_music.set_volume(0.2)
-
-background = pygame.image.load("img/background.png")
+# Задний фон
+background = pygame.image.load("background.jpg")
 background = pygame.transform.scale(background,(win_w, win_h))
+# Классы
+
+# Создание объектов и персонажа
+
+# Игровой цикл
 game = True
 while game:
+    # Второстепенная часть цикла
     window.blit(background,(0,0))
+
+    # Обязательная часть цикла
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
     pygame.display.update()
-    clock.tick(fps)
+    clock.tick(FPS)
