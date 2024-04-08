928ac4541c01e6017f67d788a6031c51925165e4
unknown <pandachka874@gmail.com>
2024-04-08 10:41:48 +0300

    upd

--- main_file.py
+++ main_file.py
@@ -2,10 +2,10 @@ from pygame import *
 from random import randint
 init()
 # Создание окна
-win_w, win_h = 700, 500
+win_w, win_h = 900, 700
 display.set_caption("Платформер")
 window=display.set_mode((win_w, win_h))
-FPS = 20
+FPS = 6
 clock = time.Clock()
 # Музыка
 mixer_music.load("backgroundMusic.mp3")
@@ -14,14 +14,6 @@ mixer_music.set_volume(0.2)
 # Задний фон
 background = image.load("background.jpg")
 background = transform.scale(background,(win_w, win_h))
-# Классы
-class Sprite():
-    def __init__(self,x,y,w,h,image):
-        self.rect = Rect(x,y,w,h)
-        image = transform.scale(image,(w,h))
-        self.image = image
-    def update(self):
-        window.blit(self.image, (self.rect.x,self.rect.y))
 walk_left = [
     image.load('mainCharJUMPorWALKleft.png'),
     image.load('mainCharleft.png')
@@ -30,6 +22,21 @@ walk_right = [
     image.load('mainCharJUMPorWALK.png'),
     image.load('mainChar.png')
 ]
+# Классы
+class Sprite():
+    def __init__(self,x,y,w,h,image):
+        self.rect = Rect(x,y,w,h)
+        image = transform.scale(image,(w,h))
+        self.image = image
+    def update(self):
+        window.blit(self.image, (self.rect.x,self.rect.y))
+class Block(Sprite):
+    def __init__(self,x,y,w,h,block_image):
+        self.image = transform.scale(image.load(block_image),(w,h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
 class Player(Sprite):
     def __init__(self,x,y,w,h,player_image,speed):
         self.image = transform.scale(image.load(player_image),(w,h))
@@ -40,7 +47,13 @@ class Player(Sprite):
         self.left = False
         self.right = False
         self.count = 0
+        
+        self.isJump = False
+        self.jumpCount = 60
+        self.fall = False
 
+        self.jump_count = 10
+        self.jump_height = self.rect.y - 150
     def move(self):
         k = key.get_pressed()
         if k[K_a] and self.rect.x >= 0:
@@ -55,19 +68,42 @@ class Player(Sprite):
             self.left = False
             self.right = False
             self.count = 0
+        if self.rect.bottom < win_h:
+            self.rect.y += 8
+            self.fall = True
+    def jump(self):
+        k = key.get_pressed()
+        if k[K_SPACE]:
+            self.isJump = True
+        if self.jump_count == 0:
+            self.isJump = False
+            self.jump_count = 60
+        if self.isJump and self.fall == False:
+            self.rect.y -= self.jump_count
+            self.jump_count -= 4
+
+
+
     def animation(self):
-        if self.count + 1 >= 20:
+        if self.count + 1 >= 6:
             self.count = 0
         if self.left == True:
-            window.blit(walk_left[self.count // 10], (self.rect.x,self.rect.y))
+            window.blit(walk_left[self.count // 3], (self.rect.x,self.rect.y))
             self.count += 1
         elif self.right == True:
-            window.blit(walk_right[self.count // 10], (self.rect.x,self.rect.y))
+            window.blit(walk_right[self.count // 3], (self.rect.x,self.rect.y))
             self.count += 1
         else:
             window.blit(self.image,(self.rect.x,self.rect.y))
 # Создание объектов и персонажа
-player = Player(20, 20, 47, 62, "mainChar.png", 2)
+player = Player(20, 400, 47, 62, "mainChar.png", 10)
+blocks = []
+my_x = 0
+for i in range(13):
+    block = Block(my_x, win_h-70, 70, 70,"block2.jpg")
+    # enemy = Enemy(randint(0, win_w-50), randint(-500, 0), 70, 40, enemy_img, randint(1, 3))
+    blocks.append(block)
+    my_x += 70
 # Игровой цикл
 pause = False
 game = True
@@ -75,9 +111,15 @@ while game:
     window.blit(background,(0,0))
     if pause == False:
         # Второстепенная часть цикла
+        for block in blocks:
+            block.update()
+            if player.rect.colliderect(block.rect):
+                player.rect.y -= 8
+                player.fall = False
         player.move()
-        # player.update()
+        player.jump()
         player.animation()
+        # player.update()
     # Обязательная часть цикла
     for e in event.get():
         if e.type == QUIT:
